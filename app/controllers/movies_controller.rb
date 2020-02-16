11a82fc3e2f0ba4827001b48570213901d6b4fcf
Siwei Cui <20461197+jncsw@users.noreply.github.com>
2020-02-16 22:17:52 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,19 +13,35 @@ class MoviesController < ApplicationController
   def index
     # @all_ratings = ['G','PG','PG-13','R']
     @all_ratings = Movie.all_ratings()
-    puts
+    puts params[:sort]
     puts params[:ratings]
     puts
+    
     @movies = Movie.all
     if not params[:ratings].nil?
       session[:ratings] = params[:ratings]
-      @ratings = params[:ratings].keys
-      # puts @ratings  #{"G"=>"1", "PG"=>"1", "PG-13"=>"1", "R"=>"1"}.keys
-      @movies = @movies.with_ratings(@ratings)
+      @ratings = params[:ratings]
+      # puts @ratings  #{"G"=>"1", "PG"=>"1", "PG-13"=>"1", "R"=>"1"}
+      @movies = @movies.with_ratings(@ratings.keys)
+
     end
     
-    if params[:sort]
+    puts '-------------------------'
+    puts session[:sort]
+    puts '-------------------------'
+    if (not params[:sort].nil?) and  params[:sort]!='none'
+      session[:sort] = params[:sort]
       @movies = @movies.order(params[:sort]).all
+    end
+    if session[:sort].nil?
+      session[:sort] = 'none'
+    end
+    puts '-------------------------'
+    puts session[:sort]
+    puts '-------------------------'
+    if params[:sort].nil?
+      flash.keep
+      redirect_to movies_path(sort: session[:sort], ratings: session[:ratings])
     end
   end
 
